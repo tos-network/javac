4a7370634ff986479d55bb629203e707c71bacd1
Tomi Setsu <xuefubaby@hotmail.com>
2025-08-10 04:58:30 +0900

    remove test files
    2025.8.10 remove test files

--- TestFloat.java
+++
@@ -1,1 +0,0 @@
-class TestFloat { public static void main(String[] args) { float f = 3.14f; double d = 2.5; System.out.println("Float: " + f + ", Double: " + d); } }
--- TestLambda.java
+++
@@ -1,1 +0,0 @@
-import java.util.*; class TestLambda { public static void main(String[] args) { List<String> list = Arrays.asList("a", "b", "c"); list.forEach(s -> System.out.println(s)); } }
